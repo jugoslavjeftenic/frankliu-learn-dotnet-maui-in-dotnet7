2e67cc6bc78184e097f4c6fb7fc1565e6ccb838a
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-07-08 17:32:50 +0200

    Clean Arhitecture - Core Business Library

---
+++ Contacts.CoreBusiness/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace Contacts.CoreBusiness;
+
+public class Contact
+{
+	public int ContactId { get; set; }
+	public string? Name { get; set; }
+	public string? Email { get; set; }
+	public string? Phone { get; set; }
+	public string? Address { get; set; }
+}
