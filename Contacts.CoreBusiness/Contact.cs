2595dd7d072afb73c05d8d7b4032ddc2283b7d13
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-07-28 17:58:51 +0200

    Configured Contact Table

--- Contacts.CoreBusiness/Contact.cs
+++ Contacts.CoreBusiness/Contact.cs
@@ -1,9 +1,18 @@
-﻿namespace Contacts.CoreBusiness;
+﻿using SQLite;
+using System.ComponentModel.DataAnnotations;
+
+namespace Contacts.CoreBusiness;
 
 public class Contact
 {
+	[Required]
+	[PrimaryKey, AutoIncrement]
 	public int ContactId { get; set; }
+
+	[Required]
 	public string? Name { get; set; }
+
+	[Required]
 	public string? Email { get; set; }
 	public string? Phone { get; set; }
 	public string? Address { get; set; }
