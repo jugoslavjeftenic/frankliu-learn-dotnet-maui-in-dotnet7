0247b1ca3629e3150e67b2ce40c253ac8d9d2478
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-07-28 17:53:29 +0200

    DataStore with SQLite

---
+++ Plugins/Contacts.Plugins.DataStore.SQLite/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Contacts.Plugins.DataStore.SQLite;
+
+public class Constants
+{
+	public const string DatabaseFileName = "ContactsSQLite.db3";
+	public static string DatabasePath =>
+		Path.Combine(FileSystem.AppDataDirectory, DatabaseFileName);
+}
