86b6600788300db9d33a38b8b9c06d7848025d83
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-07-30 18:30:09 +0200

    Configure DbContext

---
+++ Contacts.WebApi/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Contacts.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contacts.WebApi
+{
+	public class ApplicationDbContext(
+		DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+	{
+		public DbSet<Contact> Contacts { get; set; }
+	}
+}
