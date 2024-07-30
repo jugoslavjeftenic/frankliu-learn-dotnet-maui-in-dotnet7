315cbf2c7e64dc374c9f4c777bbbade7a980e1db
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-07-30 19:08:13 +0200

    Get Contacts endpoint

--- Contacts.WebApi/Program.cs
+++ Contacts.WebApi/Program.cs
@@ -11,6 +11,14 @@ builder.Services
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseHttpsRedirection();
+
+//app.UseHttpsRedirection();
+
+app.MapGet("/api/contacts", async (ApplicationDbContext db) =>
+{
+	var contacts = await db.Contacts.ToListAsync();
+
+	return Results.Ok(contacts);
+});
 
 app.Run();
